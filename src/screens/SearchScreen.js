0d6d35abf5f889c8c62f38f69d23e722703f3ca1
sami peralahti <sami.peralahti@vainu.io>
2019-08-08 16:55:57 +0300

    yelp api call with error message

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,17 +6,32 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async () => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses);
+        } catch (error) {
+            setErrorMessage('Something went wrong');
+        }
+    };
 
     return (
         <View>
             <SearchBar
                 term={term}
-                onTermChange={newTerm => setTerm(newTerm)}
-                onTermSubmit={() => {
-                    console.log('Works');
-                }}
+                onTermChange={setTerm}
+                onTermSubmit={searchApi}
             />
-            <Text>Search Screen</Text>
+            <Text>We have found {results.length} results</Text>
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
         </View>
     );
 };
